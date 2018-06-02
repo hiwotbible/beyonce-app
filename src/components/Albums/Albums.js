c413cba7f2fbb275fa6271db988abe1d879a8a51
hiwotbible <hiwotbible@gmail.com>
2018-06-02 12:10:16 +0100

    album done

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-
+import Album from "../Album/Album";
 class Albums extends Component {
+  constructor () {
+    super();
+    this.state={
+      albums:[]
+    }
+  }
+    componentDidMount() { 
+    fetch ('https://rawgit.com/rarmatei/f5ae92ac93d9716affab822a3f54f95b/raw/e62641b3f5ddd12c4fe34aa0912488224594e5a7/beyonce-albums.json')
+    .then(response => response.json())
+    .then(album => {
+      this.setState({ Albums: AlbumData })
+    })
+  }
   render() {
     return (
-      <div></div>
+      <div>
+      {this.state.albums.map(album=>(
+        <Album 
+        releaseDate={album.releaseDate}
+        album={album.collectionName}
+        imageUrl={album.artworkUrl100}
+        genre={album.primaryGenreName}/>
+      ))
+      })
+      </div>
     )
   }
 }
